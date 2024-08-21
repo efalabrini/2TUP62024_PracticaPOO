ee033e09b6c309b4f60b88abd36f77c518d043a2
PalenaFrancisco <palenafrancisco@gmail.com>
2024-08-21 15:22:07 -0300

    Practica 1

---
+++ src/Web/Controllers/Ej1controller.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PersonaController : ControllerBase
+{
+    private static readonly List<Persona> personas = new List<Persona>
+    {
+        new Persona("Juan"),
+        new Persona("Maria"),
+        new Persona("Carlos")
+    };
+
+
+    [HttpGet]
+    public ActionResult<List<string>> GetSaludo()
+    {
+        var nombres = Persona.ObtenerNombre(personas);
+        var saludos = nombres.Select(nombres => new Persona(nombres).GetSaludo()).ToList();
+        return Ok(saludos);
+    }
+
+}
---
+++ src/Web/Persona.cs
@@ -0,0 +1,28 @@
+namespace Web;
+
+public class Persona
+{
+    public string Nombre { get; set; }
+
+    public Persona(string nombre)
+    { 
+        Nombre = nombre;
+    }
+
+    public string GetSaludo()
+    {
+        return $"Hola mi nombre es {Nombre}";
+    }
+
+    public static List<string> ObtenerNombres(List<Persona> personas)
+    {
+        List<string> nombres = new List<string>();
+
+        foreach (var persona in personas)
+        {
+            nombres.Add(persona.Nombre)
+        }
+
+        return nombres;
+    }
+}
