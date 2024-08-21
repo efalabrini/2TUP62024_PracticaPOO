5f7e6e64fdba57074bbaa07741e151c5f744cf7e
Nachobastianelli <nachobastianelli2003@gmail.com>
2024-08-21 18:56:52 -0300

    Carga del ejercicio N3 POO

---
+++ src/Web/Ej3/EstudianteDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.Ej3
+{
+    public class EstudianteDto
+    {
+        public string Nombre { get; set; }
+        public int Edad {  get; set; }
+        public string Saludo { get; set; }
+    }
+}
---
+++ src/Web/Ej3/Profesor.cs
@@ -0,0 +1,17 @@
+﻿namespace Web.Ej3
+{
+    public class Profesor : Persona
+    {
+
+        public Profesor(string name) : base(name) { }
+        public string Explicar()
+        {
+            return "Estoy explicando";
+        }
+
+        public override string Saludar()
+        {
+            return $"Hola soy un profesor ({Nombre})";
+        }
+    }
+}
