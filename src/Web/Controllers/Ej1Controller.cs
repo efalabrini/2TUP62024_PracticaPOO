76972442ecd4eaa728566df2cdee09de8ed9d648
Matias D'Anunzio <matidanunzio@gmail.com>
2024-08-21 14:28:38 -0300

    ejercicio 1 realizado

---
+++ src/Web/Controllers/Ej1Controller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Ej1;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej1Controller : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult <List<string>> GetNames([FromQuery] string name1, string name2, string name3)
+        {
+          var listNames = new List<Person>()
+          {
+              new(name1),
+              new(name2),
+              new(name3)
+          };
+
+            var listSaludos = new List<string>();
+
+            foreach (var person in listNames)
+            {
+                listSaludos.Add(person.Saludo());
+            }
+
+            return listSaludos;
+        }
+        
+    }
+}
---
+++ src/Web/Ej1/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace Web.Ej1
+{
+    public class Person
+    {
+        public string? Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public string Saludo()
+        {
+            return $"Hola! mi nombre es {Name}";
+        }
+
+    }
+}
