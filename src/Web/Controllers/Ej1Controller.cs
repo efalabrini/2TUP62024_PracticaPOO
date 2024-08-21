621c59c1810d231f7f7c8da68ab03c0a0ccdfa81
Nachobastianelli <nachobastianelli2003@gmail.com>
2024-08-21 13:45:58 -0300

    Carga del ejercicio N1 de POO

---
+++ src/Web/Controllers/Ej1Controller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Ej1;
+using System.Collections.Generic;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Ej1Controller : Controller
+    {
+        [HttpGet("[action]")]
+        public ActionResult<string[]> GetSaludos([FromQuery] string name1, string name2, string name3)
+        {
+            var list = new List<Person>
+            {
+                new Person(name1),
+                new Person(name2),
+                new Person(name3),
+            };
+
+            var saludos = new List<string>();
+
+            list.ForEach(person => { saludos.Add(person.Saludo()); });
+
+            return Ok(saludos.ToArray());
+        }
+    }
+}
---
+++ src/Web/Ej1/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace Web.Ej1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Saludo()
+        {
+            return $"Hola, me llamo {Name} ;)";
+        }
+
+        public Person(string name)
+        {
+            Name = name ;
+        }
+
+    }
+    
+}
