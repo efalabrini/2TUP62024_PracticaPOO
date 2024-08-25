ad19289a94d68424d1ecd99a1600d2db90257e97
ricardobiselli <ricardobiselli@gmail.com>
2024-08-25 02:26:54 -0300

    Resolucion Ej2 POO

---
+++ src/Web/Ej2/PhotoBook.cs
@@ -0,0 +1,35 @@
+﻿//using System.ComponentModel.DataAnnotations;
+
+namespace Web.Ej2
+{
+    public class PhotoBook
+    {
+        //[Key]
+        public int Id { get; set; }
+        private static int IncrementId = 0; 
+        protected int numPages { get; set; }
+
+      
+        public PhotoBook()
+        {
+            numPages = 16;
+            Id = IncrementId++;
+        }
+
+        public PhotoBook(int pages)
+        {
+            numPages = pages;
+            Id = IncrementId++;
+
+        }
+        public int GetNumberPages()
+        {
+            return numPages;
+        }
+        public string GetFormattedDetails()
+        {
+            return $"Book ID: {Id}, Number of Pages: {numPages}";
+        }
+
+    }
+}
