f8b09f0be7260b1cc76eaceb1a0b1f9ddb3d417d
ricardobiselli <ricardobiselli@gmail.com>
2024-08-26 01:20:30 -0300

    resolucion Ej3 POO

---
+++ src/Web/Controllers/Ej3Controller.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Web.Ej3;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3Controller : ControllerBase
+    {
+        [HttpPost("CreatePerson")]
+        public IActionResult Create([FromQuery] string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                var persona = new Persona(name);
+                return Ok(persona.Saludar());
+            }
+            else { return BadRequest(); }
+
+        }
+
+        [HttpPost("createStudent")]
+        public IActionResult CreateStudent([FromQuery] string name, int edad)
+        {
+            if (!string.IsNullOrEmpty(name) && edad > 0)
+            {
+                var estudiante = new Estudiante(name);
+
+                estudiante.SetEdad(edad);
+
+                var formattedOutput = new
+                {
+                    saludo = estudiante.Saludar(),
+                    edad = estudiante.MostrarEdad(),
+                    Estudio = estudiante.Estudiar()
+                };
+
+                return Ok(formattedOutput);
+
+            }
+            else { return BadRequest(); }
+        }
+
+        [HttpPost("CreateProfessor")]
+        public IActionResult CreatProfessor([FromQuery] string name, int edad)
+        {
+            if (!string.IsNullOrEmpty(name) && edad > 0)
+            {
+                var profesor = new Profesor(name);
+
+                profesor.SetEdad(edad);
+
+                var formattedOutput = new
+                {
+                    saludo = profesor.Saludar(),
+                    Estudio = profesor.Explicar()
+                };
+
+                return Ok(formattedOutput);
+
+            }
+            else { return BadRequest(); }
+        }
+    }
+}
+
