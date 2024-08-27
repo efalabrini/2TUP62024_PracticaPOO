c772df49f2bb4648a3078fa765cc55c30aed4535
ricardobiselli <ricardobiselli@gmail.com>
2024-08-26 23:32:02 -0300

    Resolucion Ej4 POO

---
+++ src/Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using System.Net;
+using Web.Ej4;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4Controller : ControllerBase
+    {
+        [HttpPost("LoadFuelAndCreateNewCar")]
+        public IActionResult CreateCar(int fuelQuantity)
+        {
+            if (fuelQuantity >= 0)
+            {
+                var newCar = new Coche(fuelQuantity);
+                newCar.Conducir();
+                return Ok(newCar.StateMessage);
+            }
+            else 
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("LoadFuelAndCreateNewEmptyTankCar")]
+        public IActionResult CreateEmptyTankCar(int fuelQuantity)
+        {
+            if (fuelQuantity >= 0)
+            {
+                var newCar = new Coche(0);
+                newCar.CargarCombustible(fuelQuantity);
+                newCar.Conducir();
+                return Ok(newCar.StateMessage);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+    }
+}
