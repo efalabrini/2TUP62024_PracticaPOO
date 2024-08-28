6c61715ac4662aa1cb771fc3a59dc06a00d81878
Matias D'Anunzio <matidanunzio@gmail.com>
2024-08-28 19:06:30 -0300

    Ejercicio 5 resuelto

---
+++ src/Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Ej5;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5Controller : ControllerBase
+    {
+        static List<Personaje> listaPersonajes = new List<Personaje>();
+        static List<Enemigo> listaEnemigos = new List<Enemigo>();
+
+        [HttpPost("[action]")]
+        public ActionResult AddPersonajeFuerza(string nombre, int agilidad, int magia)
+        {
+            var personaje = new PersonajeDeFuerza(nombre, agilidad, magia);
+            listaPersonajes.Add(personaje);
+            return Ok($"{nombre} ha sido creado como personaje de fuerza");
+        }
+
+        [HttpPost("[action]")]
+        public ActionResult AddPersonajeAgilidad(string nombre,int fuerza, int agilidad, int magia)
+        {
+            var personaje = new PersonajeDeAgilidad(nombre, fuerza, agilidad, magia);
+            listaPersonajes.Add(personaje);
+            return Ok($"{nombre} ha sido creado como personaje de agilidad");
+        }
+
+        [HttpPost("[action]")]
+        public ActionResult AddPersonajeMagia(string nombre, int fuerza, int agilidad)
+        {
+            var personaje = new PersonajeDeMagia(nombre, fuerza, agilidad);
+            listaPersonajes.Add(personaje);
+            return Ok($"{nombre} ha sido creado como personaje de magia");
+        }
+
+        [HttpPost("[action]")]
+        public ActionResult AddEnemigo(int vida, int nivel)
+        {
+            var enemigo = new Enemigo(vida, nivel);
+            listaEnemigos.Add(enemigo);
+            return Ok($"El enemigo de nivel {nivel} se ha creado");
+        }
+
+        [HttpGet]
+        public ActionResult Batalla()
+        {
+            Turno.Atacarse(listaPersonajes);
+            return Ok("la batalla ha terminado, los personajes han atacado");
+        }
+
+
+
+    }
+}
---
+++ src/Web/Ej5/Personaje.cs
@@ -0,0 +1,31 @@
+﻿namespace Web.Ej5
+{
+    public abstract class Personaje : IMoverse
+    {
+        public string? Nombre { get; set; }
+        public int Fuerza { get; set; }
+        public int Agilidad { get; set; }
+        public int Magia { get; set; }
+
+        public abstract int CalcularDanio();
+
+        public virtual string Atacar()
+        {
+            int danio = CalcularDanio();
+            return $"{Nombre} hizo {danio} de danio";
+        }
+
+
+        public string MoverseEjeX(int distancia) 
+        {
+            return $"{Nombre} se movio {distancia} unidades en el eje x";
+        }
+
+        public string MoverseEjeY(int distancia)
+        {
+            return $"{Nombre} se movio {distancia} unidades en el eje y";
+        }
+
+
+    }
+}
