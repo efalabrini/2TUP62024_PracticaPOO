4cf7f984d2fea4e774d82f93a83c729acbeb0213
MarucoMass <mariomass23@gmail.com>
2024-08-28 00:48:08 -0300

    controlador de estudiante y profesor terminado

--- src/Web/Controllers/Ej3Controller.cs
+++ src/Web/Controllers/Ej3Controller.cs
@@ -7,21 +7,61 @@ namespace Web.Controllers;
 public class Ej3Controller : ControllerBase
 {
     [HttpPost("CreatePerson")]
-    public IActionResult CreatePerson([FromQuery] string Nombre){
-        if(Nombre == null){
+    public IActionResult CreatePerson([FromQuery] string Nombre)
+    {
+        if (Nombre == null)
+        {
             return BadRequest("Debe ingresar un nombre");
-        } else {
-        Persona persona = new Persona{
-            Nombre = Nombre
-        };
-        return Ok("Persona creada: " + persona.Saludar());
+        }
+        else
+        {
+            Persona persona = new Persona
+            {
+                Nombre = Nombre
+            };
+            return Ok(persona.Saludar());
         }
     }
 
     [HttpPost("createStudent")]
     public IActionResult CreateStudent([FromQuery] string Nombre, [FromQuery] int edad)
     {
-        
+        if (Nombre == null)
+        {
+            return BadRequest("Debe ingresar un nombre");
+        }
+        if (edad <= 0)
+        {
+            return BadRequest("Debe ingresar una edad mayor a 0");
+        }
+
+        Estudiante persona = new Estudiante
+        {
+            Nombre = Nombre
+        };
+        persona.SetEdad(edad);
+        return Ok(persona.Saludar() + ". " + persona.MostrarEdad());
+    }
+
+    [HttpPost("createProfessor")]
+    public IActionResult CreateProfessor([FromQuery] string Nombre, [FromQuery] int edad)
+    {
+        if (Nombre == null)
+        {
+            return BadRequest("Debe ingresar un nombre");
+        }
+        if (edad <= 0)
+        {
+            return BadRequest("Debe ingresar una edad mayor a 0");
+        }
+
+        Profesor profesor = new Profesor
+        {
+            Nombre = Nombre
+        };
+        profesor.SetEdad(edad);
+        return Ok(profesor.Saludar() + ". " + profesor.Explicar());
+
     }
 }
 
