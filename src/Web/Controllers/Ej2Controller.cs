35eefd61d7486f7454d142e6de070de1547f7fc2
Ren991 <beccrenzo@gmail.com>
2024-08-28 00:12:17 -0300

    Resolví ejercicio 2 POO

---
+++ src/Web/Controllers/Ej2Controller.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Ej2;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Ej2Controller : Controller
+    {
+        private static List<Photobook> albums = new List<Photobook>();
+
+
+        [HttpPost("standard")]
+        public IActionResult CreateStandardPhotoBook([FromQuery] int? pages)
+        {
+            Photobook album;
+
+            if (pages.HasValue)
+            {
+                album = new Photobook(pages.Value);
+            }
+            else
+            {
+                album = new Photobook();
+                pages = album.GetNumberPages();
+            }
+
+            albums.Add(album);
+            return Ok("El album creado tiene " + pages + " paginas");
+        }
+
+        
+        [HttpPost("largePhotoBook")]
+        public IActionResult CreateLargePhotoBook()
+        {
+            var album = new BigPhotoBook();
+            albums.Add(album);
+            return Ok();
+        }
+
+        [HttpGet("pages /{id}")]
+        public IActionResult GetNumberOfPages(int id)
+        {
+            var album = albums.ElementAtOrDefault(id - 1);
+            if(album == null)
+            {
+                return NotFound("Album no encontrado");
+            }
+
+            return Ok("El album encontrado tiene " + album.GetNumberPages() + " páginas.");
+        }
+
+        [HttpGet("getAll")]
+        public IActionResult GetAllAlbums()
+        {
+            var result = albums.Select((album, index) => new { Id = index + 1, Pages = album.GetNumberPages() }).ToList();
+            return Ok(result);
+        }
+    }
+}
---
+++ src/Web/Ej2/Photobook.cs
@@ -0,0 +1,22 @@
+﻿namespace Web.Ej2
+{
+    public class Photobook
+    {
+        protected int numPages; // atributo protegido
+
+        public Photobook()
+        {
+            numPages = 16; // por defecto arranca con 16
+        }
+
+        public Photobook(int pages)
+        {
+            this.numPages = pages; // se especifica cantiad.
+        }
+
+        public int GetNumberPages()
+        {
+            return numPages; // retorna numero de páginas.
+        }
+    }
+}
