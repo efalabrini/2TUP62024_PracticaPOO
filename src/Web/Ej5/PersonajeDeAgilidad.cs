60bcc5b90faacc10f320139692171e515f5f0c78
Ren991 <beccrenzo@gmail.com>
2024-08-30 15:20:40 -0300

    resolví ejercicio 5 POO

---
+++ src/Web/Ej5/PersonajeDeAgilidad.cs
@@ -0,0 +1,20 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Web.Ej5
+{
+    public class PersonajeDeAgilidad: Personaje
+    {
+        public PersonajeDeAgilidad(string nombre, int fuerza, int agilidad, int magia)
+        {
+            Nombre = nombre;
+            Fuerza = fuerza;
+            Agilidad = agilidad;
+            Magia = magia;
+        }
+
+        public override int CalcularDanio()
+        {
+            return (Fuerza / 2) * Agilidad * (Magia / 2);
+        }
+    }
+}
