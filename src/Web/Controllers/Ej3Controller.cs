9ac506a1c4a0454cee85fa5dc4d6cfa80e299ca5
Danilo Mercado <mercadodanilo213@hotmail.com>
2024-09-05 20:24:37 -0300

    Ej3POO

---
+++ src/Web/Controllers/Ej3Controller.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Ej3;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3Controller : ControllerBase 
+    {
+        [HttpGet("SaludarP")]
+        public IActionResult GetSaludo() 
+        {
+            var persona = new Persona("Dilan");
+
+            return Ok(persona.Saludar());
+        }
+
+        [HttpGet("SaludarEst")]
+        public ActionResult<Estudiante> GetSaludarEstudiante([FromQuery] string name, int edad) 
+        {
+            var estudiante = new Estudiante(name);
+            estudiante.setEdad(edad);
+
+            var saludarEstudiante = estudiante.Saludar();
+            var edadEstudiante = estudiante.MostrarEdad();
+
+            return Ok($"{saludarEstudiante}, {edadEstudiante}");
+        }
+
+        [HttpGet("SaludarProfe")]
+        public ActionResult<Profesor> GetSaludarProfesor([FromQuery] string name, int edad)
+        {
+            var profesor = new Profesor(name);
+            profesor.setEdad(edad);
+
+            var saludarProfesor = profesor.Saludar();
+            var explicacionProfesor = profesor.Explicar();
+
+            return Ok($"{saludarProfesor}, {explicacionProfesor}, {edad}");
+
+        }
+    }
+}
---
+++ src/Web/Ej3/Persona.cs
@@ -0,0 +1,24 @@
+﻿namespace Web.Ej3
+{
+    public class Persona
+    {
+        public int Edad { get; set; }
+        public string Nombre { get; set; }
+
+        public string Saludo { get; set; }
+
+        public Persona(string nombre)
+        {
+            Nombre = nombre;
+        }
+        public void setEdad(int edad) 
+        {
+            Edad = edad;
+        }
+
+        public string Saludar()
+        {
+            return "Hola";
+        }
+    }
+}
