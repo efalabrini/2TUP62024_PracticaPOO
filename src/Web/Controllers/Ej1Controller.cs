1724b306ee4e4e1a1d52e94e20b017385bb263e4
Juan Martin Masqueda <j.masqueda@gmail.com>
2024-09-06 10:11:53 -0300

    Resuelto ej1 POO

---
+++ src/Web/Controllers/Ej1Controller.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using Web.Ej1;
+
+namespace Web.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class Ej1Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] string name1, string name2, string name3)
+        {
+
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona(name1));
+            personas.Add(new Persona(name2));
+            personas.Add(new Persona(name3));
+
+
+            List<string> saludos = [];
+
+            foreach (Persona persona in personas)
+            {
+                saludos.Add(persona.GetSaludo());
+            }
+
+            return Ok(saludos);
+        }
+
+        
+    }
+}
---
+++ src/Web/Ej1/Persona.cs
@@ -0,0 +1,21 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Web.Ej1
+{
+    public class Persona
+    {
+
+        public string? Name{ get; set; }
+
+        public Persona(string name) { 
+        
+            Name = name;
+        }
+
+
+        public string GetSaludo()
+        {
+            return $"Hola! mi nombre es {Name}";
+        }
+    }
+}
