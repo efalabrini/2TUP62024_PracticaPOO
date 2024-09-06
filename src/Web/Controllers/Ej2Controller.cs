76592ddd9ff6227f1e9907a51b5524101df8ff97
Juan Martin Masqueda <j.masqueda@gmail.com>
2024-09-06 10:51:41 -0300

    Resolvi ej 2

---
+++ src/Web/Controllers/Ej2Controller.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Ej2;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej2Controller : ControllerBase
+    {
+
+        static List<PhotoBook> listAlbums = new List<PhotoBook>();
+
+
+        [HttpPost("PhotoBook")]
+        public IActionResult PhotoBook ( int? numPages = null)
+        {
+
+            PhotoBook photoBook = numPages.HasValue ? new PhotoBook(numPages.Value) : new PhotoBook();
+
+            listAlbums.Add(photoBook);
+
+            return Ok(photoBook.GetNumberPages());
+        }
+
+
+        [HttpPost("BigPhotoBook")]
+        public IActionResult BigPhotoBook()
+        {
+            BigPhotoBook bigPhotoBook = new BigPhotoBook();
+            listAlbums.Add(bigPhotoBook);
+
+            return Ok(bigPhotoBook.GetNumberPages());
+        }
+
+        [HttpGet("PhotoBookLength")]
+        public IActionResult PhotoBookLength(int id)
+        {
+            var photoBook = listAlbums.FirstOrDefault(i => i.id == id);
+
+            if (photoBook != null)
+            {
+                return Ok(photoBook.GetNumberPages());
+            }
+
+            return StatusCode(404);
+
+        }
+
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll() 
+        {
+            var result = new List<object>();
+
+            foreach (var book in listAlbums)
+            {
+                result.Add(new
+                {
+                    BookId = book.id,
+                    PageCount = book.GetNumberPages()
+                });
+            }
+
+
+            return Ok(result); 
+        }
+    }
+}
---
+++ src/Web/Ej2/PhotoBook.cs
@@ -0,0 +1,24 @@
+﻿namespace Web.Ej2
+{
+    public class PhotoBook
+    {
+        private static int _albumId = 0;
+        protected int numPages { get; set; }
+        public int id { get; set; }
+        public PhotoBook()
+        {
+            numPages = 16;
+            id = _albumId++;
+        }
+
+        public PhotoBook(int pages)
+        {
+            numPages = pages;
+            id = _albumId++;
+        }
+        public int GetNumberPages()
+        {
+            return numPages;
+        }
+    }
+}
