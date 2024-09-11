6e15d2702be721e0d3934c81dedadd8976af623f
Franco Berlochi <francoberlochi@gmail.com>
2024-09-11 19:10:21 -0300

    Eje2Poo terminado

--- src/Web/Controllers/Eje2Controller.cs
+++ src/Web/Controllers/Eje2Controller.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web.Eje2;
 
 namespace Web.Controllers;
 [ApiController]
@@ -6,10 +7,56 @@ namespace Web.Controllers;
 
     public class Eje2Controller : Controller
     {
-        [HttpGet]
-        public IActionResult Index()
+        private static List<PhotoBook> albums = new List<PhotoBook>();
+
+        [HttpPost("createStandarAlbum")]
+        public IActionResult CreateStandarAlbum(int? numPages)
         {
-            return View();
+            PhotoBook album;
+            if (numPages.HasValue)
+            {
+                album = new PhotoBook(numPages.Value);
+            }
+            else 
+            {
+                album = new PhotoBook();
+            }
+            albums.Add(album);
+            return Ok("Album estandar creado con éxito");
+        }
+
+        [HttpPost("createBigAlbum")]
+        public IActionResult CreateBigAlbum()
+        {
+            var album = new BigPhotoBook();
+            albums.Add(album);
+            return Ok("Album grande creado con éxito");
+        }
+
+        [HttpGet("checkNumberPages")]
+        public IActionResult CheckNumberPages([FromQuery] int id) 
+        {
+            int i;
+            if (albums.Count == 0) {return NotFound("No hay álbumes existentes");}
+
+            else if (id <= 0) { return BadRequest("Id inválida"); }
+
+            for (i = 0; i < albums.Count; i++)
+            {
+                //busca el album que coincida con la id
+                if (albums[i].Id == id) { break; }
+            }
+
+            //si no se encontro en el bucle se muestra este msg
+            if (i == albums.Count) { return NotFound($"No se ah encontrado el album con la id: {id}"); }
+
+            return Ok(albums[i].GetNumberPages());
+        }
+
+        [HttpGet("returnAlbums")]
+        public IActionResult ReturnAlbums() 
+        { 
+            return Ok(albums);
         }
     }
 
--- src/Web/Eje2/PhotoBook.cs
+++ src/Web/Eje2/PhotoBook.cs
@@ -2,5 +2,30 @@
 {
     public class PhotoBook
     {
+
+        protected static int AutoincrementId = 0;  
+        
+        //Si se deja en protected no se pueden ver los álbumes, para ver poner en public NumPages 
+        protected int NumPages {  get; set; }
+        public int Id { get; set; }
+
+      
+
+        public PhotoBook()
+        {
+            NumPages = 16;
+            AutoincrementId++;
+            Id = AutoincrementId;
+        }
+
+        public PhotoBook(int numPages)
+        {
+            NumPages = numPages;
+            AutoincrementId++;
+            Id = AutoincrementId;
+        }
+
+        public int GetNumberPages() {  return NumPages; }
+
     }
 }
