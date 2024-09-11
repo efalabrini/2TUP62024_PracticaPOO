adc739e205ebe887eb84a640d412ac7612d81e11
mendieta967 <olorteguopedro@gmail.com>
2024-09-11 20:56:06 -0300

    Ej03Resuelto

---
+++ src/Web/Controllers/Ejercicio03/Profesor.cs
@@ -0,0 +1,17 @@
+﻿public class Profesor : Persona
+{
+
+    public Profesor(string nombre) : base(nombre) { }
+
+
+    public string Explicar()
+    {
+        return "Estoy explicando";
+    }
+
+
+    public override string Saludar()
+    {
+        return $"Hola soy el profesor {Nombre}";
+    }
+}
