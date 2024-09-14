7aaaf404ffbb46aaa770406e15c828275e776e75
Franco <cfranco3102@gmail.com>
2024-09-14 18:58:33 -0300

    Ejercicio 3 resuelto

---
+++ src/Web/Ej3/Estudiante.cs
@@ -0,0 +1,21 @@
+﻿namespace Web.Ej3
+{
+    public class Estudiante : Persona 
+    {
+
+        public Estudiante(int edad, string nombre) : base(edad, nombre)
+        {
+            
+        }
+
+        public string Estudiar() { return "Estoy estudiando"; }
+
+        public string MostrarEdad()
+        {
+            return $"Mi edad es: {Edad} años";
+        }
+        public string Saludar() { return $"Hola soy el estudiante {Nombre}"; }
+
+
+    }
+}
---
+++ src/Web/Ej3/Profesor.cs
@@ -0,0 +1,18 @@
+﻿namespace Web.Ej3
+{
+    public class Profesor : Persona
+    {
+        public Profesor(int edad, string nombre) : base(edad, nombre)
+        {
+
+        }
+        public string Explicar()
+        {
+            return "Estoy explicando";
+        }
+
+        public string Saludar() { 
+            return $"Hola soy el profesor {Nombre}"; 
+        }
+    }
+}
