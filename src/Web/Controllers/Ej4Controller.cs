da5574abbae4577c7b443edf8cff80a63d86dd56
Agustin <agustinferraresi5@gmail.com>
2024-09-14 14:41:50 -0300

    Ej4 resuelto

---
+++ src/Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Web.Classes;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Ej4Controller : Controller
+    {
+        [HttpPost("[action]")]
+        public IActionResult CarCreate1([FromQuery] float fuel)
+        {
+            Car car = new Car(fuel);
+            return Ok($"{car.drive()} y tiene {car.Getfuel()} litros de nafta");
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult CreateCar2([FromQuery] float fuel)
+        {
+            Car car = new Car(0);
+            car.refuel(fuel);
+            return Ok($"{car.drive()} y tiene {car.Getfuel()} litros de nafta");
+        }
+    }
+}
