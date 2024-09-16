a4fea4e618087bc7254b0c13156aeb020f8c14a6
Agustin <agustinferraresi5@gmail.com>
2024-09-15 21:05:29 -0300

    Ej5 Resuelto

---
+++ src/Web/Classes/Character.cs
@@ -0,0 +1,44 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using Web.Interfaces;
+
+namespace Web.Classes
+{
+    public abstract class Character : IMove
+    {
+        protected string Name { get; set; }
+        protected float Strength { get; set; }
+        protected float Agility { get; set; }
+        protected float Magic { get; set; }
+
+        public Character(string name, float strength, float agility, float magic)
+        {
+            Name = name;
+            Strength = strength;
+            Agility = agility;
+            Magic = magic;
+        }
+
+        protected abstract float damageCalculate();
+
+
+        public virtual string attack()
+        {
+            return $"El personaje {Name} atacó, haciendo {damageCalculate()} de daño";
+        }
+
+        public string moveX(int x)
+        {
+            return $"El personaje {Name} se movió en el eje X a la posicion {x}";
+        }
+
+        public string moveY(int y)
+        {
+            return $"El personaje {Name} se movió en el eje Y a la posicion {y}";
+        }
+
+        public string name => Name;
+        public float strength => Strength;
+        public float agility => Agility;
+        public float magic => Magic;
+    }
+}
---
+++ src/Web/Classes/MagicCharacter.cs
@@ -0,0 +1,27 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Web.Classes
+{
+    public class MagicCharacter : Character
+    {
+        public MagicCharacter(string name, float strength, float agility, float magic) : base(name, strength, agility, magic)
+        {
+        }
+
+        public MagicCharacter(string name, float strength, float agility) : base(name, strength, agility,60)
+        {
+            
+        }
+
+        protected override float damageCalculate()
+        {
+            float damage = strength + agility + magic * 4;
+            return damage;
+        }
+
+        public override string attack()
+        {
+            return $"{name} hizo {damageCalculate()} de daño gracias a su magia";
+        }
+    }
+}
